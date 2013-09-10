1a23f4f71b67cd4b0d054f7c843689efe28efe14
JD <johndavid400@gmail.com>
2013-09-09 19:11:00 -0500

    updated to working and tested

--- motor_shield_rc/motor_shield_rc.ino
+++ motor_shield_rc/motor_shield_rc.ino
@@ -1,4 +1,3 @@
-
 // Connect RC 1 to A0 and RC 2 to A1.
 // Mount Arduino Motor Controller Shield
 
@@ -13,11 +12,17 @@ int pwm_2 = 11;
 int disable_2 = 8;
 
 // create a threshold for neutral values - the higher this value, the larger the neutral band will be
-int threshold = 10;
+int threshold = 20;
+
+// set values for high and low R/C raw values
+int rc1_low = 1250;
+int rc1_high = 1730;
+int rc2_low = 1250;
+int rc2_high = 1750;
 
 // declare R/C inputs
-int rc1 = 14;
-int rc2 = 15;
+int rc1 = 16;  // use A2
+int rc2 = 17;  // use A3
 
 int rc1_val = 0;
 int rc2_val = 0;
@@ -40,42 +45,11 @@ void setup(){
   // setup input pins for R/C
   pinMode(INPUT, rc1);
   pinMode(INPUT, rc2);
-
-  test_motors();
-  delay(1000);
-}
-
-void test_motors(){
-  rc1_speed = 128;
-  rc2_speed = 128;
-  m1_forward();
-  m2_forward();
-  delay(1000);
-  m1_stop();
-  m2_stop();
-  delay(1000);
-  m1_reverse();
-  m2_reverse();
-  delay(1000);
-  m1_stop();
-  m2_stop();
-  delay(1000);
-  m1_reverse();
-  m2_forward();
-  delay(1000);
-  m1_stop();
-  m2_stop();
-  delay(1000);
-  m1_forward();
-  m2_reverse();
-  delay(1000);
-  m1_stop();
-  m2_stop();
-  delay(1000);
 }
 
 void loop(){
  read_rc();
+ limit_speed();
  write_motors();
  serial_print_stuff();
 }
@@ -83,25 +57,50 @@ void loop(){
 void read_rc(){
  // read and map rc1
  rc1_val = pulseIn(rc1, HIGH, 20000);
- rc1_speed = map(rc1_val, 1000, 2000, -255, 255);
+ if(rc1_val > 0){
+   rc1_speed = map(rc1_val, rc1_low, rc1_high, -255, 255);
+ }
+ else{
+   rc1_speed = 0;
+ }
  // read and map rc2
  rc2_val = pulseIn(rc2, HIGH, 20000);
- rc2_speed = map(rc2_val, 1000, 2000, -255, 255);
+ if(rc2_val > 0){
+   rc2_speed = map(rc2_val, rc2_low, rc2_high, -255, 255);
+ }
+ else{
+   rc2_speed = 0;
+ }
 }
 
 void serial_print_stuff(){
  // print values for rc1
- Serial.print("  RC1 Raw: ");
+ Serial.print(" RC1 Raw: ");
  Serial.print(rc1_val);
- Serial.print("  RC1 Adj: ");
+ Serial.print(" RC1 Adj: ");
  Serial.print(rc1_speed);
  // print values for rc1
- Serial.print("  RC2 Raw: ");
- Serial.print(rc2_speed);
- Serial.print("  RC2 Adj: ");
+ Serial.print(" RC2 Raw: ");
  Serial.print(rc2_val);
+ Serial.print(" RC2 Adj: ");
+ Serial.print(rc2_speed);
  // print new line
  Serial.println("");
+}
+
+void limit_speed(){
+ if(rc1_speed > 255){
+   rc1_speed = 255;
+ }
+ else if(rc1_speed < -255){
+   rc1_speed = -255; 
+ }
+ if(rc2_speed > 255){
+   rc2_speed = 255;
+ }
+ else if(rc2_speed < -255){
+   rc2_speed = -255; 
+ }
 }
 
 void write_motors(){
@@ -127,33 +126,30 @@ void write_motors(){
 }
 
 void m1_forward(){
-  digitalWrite(disable_1, LOW);
   digitalWrite(dir_1, LOW);
   analogWrite(pwm_1, rc1_speed);
 }
 
 void m1_reverse(){
-  digitalWrite(disable_1, LOW);
   digitalWrite(dir_1, HIGH);
   analogWrite(pwm_1, -rc1_speed);
 }
 
 void m1_stop(){
   digitalWrite(pwm_1, LOW);
-  digitalWrite(disable_1, HIGH);
 }
 
 void m2_forward(){
-  digitalWrite(disable_2, LOW);
   digitalWrite(dir_2, LOW);
   analogWrite(pwm_2, rc2_speed);
 }
-void m2_reverse(){ digitalWrite(disable_2, LOW);
+void m2_reverse(){ 
   digitalWrite(dir_2, HIGH);
   analogWrite(pwm_2, -rc2_speed);
 }
 
 void m2_stop(){
   digitalWrite(pwm_2, LOW);
-  digitalWrite(disable_2, HIGH);
 }
+
+
