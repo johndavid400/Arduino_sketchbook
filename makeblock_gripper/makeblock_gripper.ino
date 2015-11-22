13a2c73cad0a04bdfb62fcf5b1144f54213373a9
J.D. Warren <johndavid400@gmail.com>
2015-11-21 22:49:09 -0600

    update gripper code - now working

--- makeblock_gripper/makeblock_gripper.ino
+++ makeblock_gripper/makeblock_gripper.ino
@@ -4,6 +4,10 @@ int rc1 = 14;
 int rc2 = 15;
 int rc3 = 16;
 
+int rc1_raw = 0;
+int rc2_raw = 0;
+int rc3_raw = 0;
+
 int rc1_val = 0;
 int rc2_val = 0;
 int rc3_val = 0;
@@ -13,27 +17,27 @@ int m2_out = 0;
 int m3_out = 0;
 int m4_out = 0;
 
-int M1L = 7;
-int M1R = 8;
-int M1P = 9;
+int M1L = 13;
+int M1R = 12;
+int M1P = 10;
 
-int M2L = 13;
-int M2R = 12;
-int M2P = 10;
+int M2L = 8;
+int M2R = 7;
+int M2P = 9;
 
-int M3L = 2;
-int M3R = 4;
+int M3L = 4;
+int M3R = 2;
 int M3P = 3;
 
-int M4L = 5;
-int M4R = 6;
+int M4L = 6;
+int M4R = 5;
 int M4P = 11;
 
 // some extra stuff
 int deadband = 20;
 int max_spd = 255;
-int rc_min = 1050;
-int rc_max = 1950;
+int rc_min = 1500;
+int rc_max = 2600;
 int motor_speed = 0;
 int motor_bias = 0;
 
@@ -63,22 +67,17 @@ void setup(){
 
 void loop(){
   read_inputs();
-  limit();
-  set_motors();
   write_motors();
+  print_stuff();
 }
 
 void read_inputs(){
-  rc1_val = map(pulseIn(rc1, HIGH, 20000), rc_min, rc_max, -max_spd, max_spd);
-  rc2_val = map(pulseIn(rc2, HIGH, 20000), rc_min, rc_max, -max_spd, max_spd);
-  rc3_val = map(pulseIn(rc3, HIGH, 20000), rc_min, rc_max, -max_spd, max_spd);
-}
-
-void limit(){
-  if (rc1_val > max_spd){rc1_val = max_spd;}
-  else if (rc1_val < -max_spd){rc1_val = -max_spd;}
-  if (rc2_val > max_spd){rc2_val = max_spd;}
-  else if (rc2_val < -max_spd){rc2_val = -max_spd;}
+  rc1_raw = pulseIn(rc1, HIGH, 20000);
+  rc2_raw = pulseIn(rc2, HIGH, 20000);
+  rc3_raw = pulseIn(rc3, HIGH, 20000);
+  rc1_val = map(rc1_raw, rc_min, rc_max, -max_spd, max_spd);
+  rc2_val = map(rc2_raw, rc_min, rc_max, -max_spd, max_spd);
+  rc3_val = map(rc3_raw, 1300, 2600, -max_spd, max_spd);
 }
 
 void set_motors(){
@@ -96,7 +95,20 @@ void set_motors(){
   }
 }
 
+void limit(){
+  if (m1_out > max_spd){m1_out = max_spd;}
+  else if (m1_out < -max_spd){m1_out = -max_spd;}
+  if (m2_out > max_spd){m2_out = max_spd;}
+  else if (m2_out < -max_spd){m2_out = -max_spd;}
+  if (m3_out > max_spd){m3_out = max_spd;}
+  else if (m3_out < -max_spd){m3_out = -max_spd;}
+  if (m4_out > max_spd){m4_out = max_spd;}
+  else if (m4_out < -max_spd){m4_out = -max_spd;}
+} 
+
 void write_motors(){
+  set_motors();
+  limit();
   if (rc3_val > 0) {
     if (m1_out > deadband) {left_forward();}
     else if (m1_out < -deadband) {left_reverse();}
@@ -121,7 +133,7 @@ void print_stuff(){
  Serial.print("  RC 2: ");
  Serial.print(rc2_val);
  Serial.print("  RC 3: ");
- Serial.print(rc2_val);
+ Serial.print(rc3_val);
  Serial.print("  Left: ");
  Serial.print(m1_out);
  Serial.print("  Right: ");
@@ -141,7 +153,7 @@ void left_forward(){
 void left_reverse(){
   digitalWrite(M1R, LOW);
   digitalWrite(M1L, HIGH);
-  analogWrite(M1P, m1_out);
+  analogWrite(M1P, -m1_out);
 }
 void left_stop(){
   digitalWrite(M1P, LOW);
@@ -155,7 +167,7 @@ void right_forward(){
 void right_reverse(){
   digitalWrite(M2R, LOW);
   digitalWrite(M2L, HIGH);
-  analogWrite(M2P, m2_out);
+  analogWrite(M2P, -m2_out);
 }
 void right_stop(){
   digitalWrite(M2P, LOW);
@@ -169,7 +181,7 @@ void lift_forward(){
 void lift_reverse(){
   digitalWrite(M3R, LOW);
   digitalWrite(M3L, HIGH);
-  analogWrite(M3P, m3_out);
+  analogWrite(M3P, -m3_out);
 }
 void lift_stop(){
   digitalWrite(M3P, LOW);
@@ -183,7 +195,7 @@ void claw_forward(){
 void claw_reverse(){
   digitalWrite(M4R, LOW);
   digitalWrite(M4L, HIGH);
-  analogWrite(M4P, m4_out);
+  analogWrite(M4P, -m4_out);
 }
 void claw_stop(){
   digitalWrite(M4P, LOW);
