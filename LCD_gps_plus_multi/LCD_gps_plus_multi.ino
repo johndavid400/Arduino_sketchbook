c404da91512d82f71dc3a4932f3d52611f1192de
JD <johndavid400@gmail.com>
2016-02-14 17:07:25 -0600

    fix directional bug

--- LCD_gps_plus_multi/LCD_gps_plus_multi.ino
+++ LCD_gps_plus_multi/LCD_gps_plus_multi.ino
@@ -23,9 +23,8 @@ void setup()
 void loop(){
   checkPot();
   if (potVal < 300){
-    Serial.println(potVal);
     if (gps.location.isUpdated()){
-      lcd.clear(); 
+      lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Lat: ");
       lcd.print(gps.location.lat(), 6);
@@ -36,7 +35,7 @@ void loop(){
   }
   else if (potVal >= 300 && potVal < 600){
     if (gps.location.isUpdated()){
-      lcd.clear(); 
+      lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Time: ");
       lcd.print(gps.time.hour());
@@ -53,7 +52,7 @@ void loop(){
   else if (potVal >= 600 && potVal < 900){
     if (gps.location.isUpdated()){
       setDirection();
-      lcd.clear(); 
+      lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Course: ");
       lcd.setCursor(0, 1);
@@ -64,7 +63,7 @@ void loop(){
   }
   else {
     checkTemp();
-    lcd.clear(); 
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
     lcd.print(tempF);
@@ -77,7 +76,7 @@ static void smartdelay(unsigned long ms) {
   do {
     while (ss.available())
       gps.encode(ss.read());
-  } 
+  }
   while (millis() - start < ms);
 }
 
@@ -85,7 +84,7 @@ void checkTemp(){
   voltage = analogRead(3) * 5.0;
   voltage /= 1024.0;
   tempC = (voltage - 0.5) * 100;
-  tempF = (tempC * 9.0 / 5.0) + 32.0; 
+  tempF = (tempC * 9.0 / 5.0) + 32.0;
 }
 
 void checkPot(){
@@ -93,31 +92,16 @@ void checkPot(){
 }
 
 void setDirection(){
-  float courseVal = gps.course.value();
-  if (courseVal >= 22.5 && courseVal < 67.5) {
-    bearing = "NE";
-  }
-  else if (courseVal >= 67.5 && courseVal < 112.5) {
-    bearing = "E";
-  }
-  else if (courseVal >= 112.5 && courseVal < 157.5) {
-    bearing = "SE";
-  }
-  else if (courseVal >= 157.5 && courseVal < 202.5) {
-    bearing = "S";
-  }
-  else if (courseVal >= 202.5 && courseVal < 247.5) {
-    bearing = "SW";
-  }
-  else if (courseVal >= 247.5 && courseVal < 292.5) {
-    bearing = "W";
-  }
-  else if (courseVal >= 292.5 && courseVal < 337.5) {
-    bearing = "W";
-  }
-  else {
-    bearing = "N"; 
-  }
+  float courseVal = gps.course.deg();
+  if (courseVal >= 22.5 && courseVal < 67.5) { bearing = "NE"; }
+  else if (courseVal >= 67.5 && courseVal < 112.5) { bearing = "E"; }
+  else if (courseVal >= 112.5 && courseVal < 157.5) { bearing = "SE";}
+  else if (courseVal >= 157.5 && courseVal < 202.5) { bearing = "S"; }
+  else if (courseVal >= 202.5 && courseVal < 247.5) { bearing = "SW"; }
+  else if (courseVal >= 247.5 && courseVal < 292.5) { bearing = "W"; }
+  else if (courseVal >= 292.5 && courseVal < 337.5) { bearing = "W"; }
+  else { bearing = "N"; }
 }
+
 
 
