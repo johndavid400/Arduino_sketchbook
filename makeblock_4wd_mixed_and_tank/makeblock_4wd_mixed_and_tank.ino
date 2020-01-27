0c29fc1ee1067fb5a73482a34915263899c39d0f
johndavid400 <johndavid400@gmail.com>
2020-01-26 21:11:15 -0600

    makeblock 4wd - add mixed-mode and tank-mode into same sketch

--- makeblock_4wd_mixed_and_tank/makeblock_4wd_mixed_and_tank.ino
+++ makeblock_4wd_mixed_and_tank/makeblock_4wd_mixed_and_tank.ino
@@ -1,10 +1,21 @@
 
-// declare R/C inputs
+// declare R/C inputs for tank mode
 int rc_left = 14;
 int rc_right = 15;
 int rc_left_val = 0;
 int rc_right_val = 0;
 
+// declare R/C inputs for mixed mode
+int rc_speed = 15;
+int rc_turn = 16;
+int rc_speed_val = 0;
+int rc_turn_val = 0;
+int motor_speed = 0;
+int motor_bias = 0;
+
+// declare other variables
+int pulse = 0;
+int mode = 0;
 int deadband = 20;
 int right_spd = 0;
 int left_spd = 0;
@@ -51,26 +62,40 @@ void setup() {
   pinMode(OUTPUT, motor_right_b1);
   pinMode(OUTPUT, motor_right_b2);
   pinMode(OUTPUT, motor_right_b_pwm);
-  delay(1500);
+  delay(2500);
+  
+  if ( pulseIn(rc_turn, HIGH) > 1700 ) {
+    mode = 1;
+    Serial.println(pulseIn(rc_turn, HIGH));
+    Serial.println(mode);
+  }
 }
 
 void loop() {
   read_inputs();
   interpolate();
+  limit();
   write_motors();
   print_stuff();
 }
 
 void read_inputs() {
-  rc_left_val = pulseIn(rc_left, HIGH);
-  rc_right_val = pulseIn(rc_right, HIGH);
+  if (mode == 0) {
+    rc_speed_val = pulseIn(rc_speed, HIGH);
+    rc_turn_val = pulseIn(rc_turn, HIGH);
+    pulse = rc_speed_val;
+  }
+  else {
+    rc_left_val = pulseIn(rc_left, HIGH);
+    rc_right_val = pulseIn(rc_right, HIGH);
+    pulse = rc_right_val;
+  }
 }
 
 void interpolate() {
-  if (rc_left_val > 500 && rc_right_val > 500) {
-    left_spd = map(rc_left_val, rc_min, rc_max, -max_spd, max_spd);
-    right_spd = map(rc_right_val, rc_min, rc_max, -max_spd, max_spd);
-    limit();
+  if (pulse > 500) {
+    if (mode == 0) { mixed_mode(); }
+    else { tank_mode(); }
   }
   else {
     left_spd = 0;
@@ -78,49 +103,50 @@ void interpolate() {
   }
 }
 
+void tank_mode() {
+  left_spd = map(rc_left_val, rc_min, rc_max, -max_spd, max_spd);
+  right_spd = map(rc_right_val, rc_min, rc_max, -max_spd, max_spd);
+}
+
+void mixed_mode() {
+  motor_speed = map(rc_speed_val, rc_min, rc_max, -max_spd, max_spd);
+  motor_bias = map(rc_turn_val, rc_min, rc_max, -max_spd, max_spd);
+  left_spd = motor_speed - motor_bias;
+  right_spd = motor_speed + motor_bias;
+}
+
 void limit() {
-  if (left_spd > max_spd) {
-    left_spd = max_spd;
-  }
-  else if (left_spd < -max_spd) {
-    left_spd = -max_spd;
-  }
-  if (right_spd > max_spd) {
-    right_spd = max_spd;
-  }
-  else if (right_spd < -max_spd) {
-    right_spd = -max_spd;
-  }
+  if (left_spd > max_spd){left_spd = max_spd;}
+  else if (left_spd < -max_spd){left_spd = -max_spd;}
+  if (right_spd > max_spd){right_spd = max_spd;}
+  else if (right_spd < -max_spd){right_spd = -max_spd;}
 }
 
 void write_motors() {
-  if (left_spd > deadband) {
-    left_forward();
-  }
-  else if (left_spd < -deadband) {
-    left_reverse();
-  }
-  else {
-    left_stop();
-  }
-  if (right_spd > deadband) {
-    right_forward();
-  }
-  else if (right_spd < -deadband) {
-    right_reverse();
-  }
-  else {
-    right_stop();
-  }
+  if (left_spd > deadband) {left_forward();}
+  else if (left_spd < -deadband) {left_reverse();}
+  else {left_stop();}
+  if (right_spd > deadband) {right_forward();}
+  else if (right_spd < -deadband) {right_reverse();}
+  else {right_stop();}
 }
 
 void print_stuff() {
-  //Serial.print("  Left: ");
-  //Serial.print(rc_left_val);
-  Serial.print(left_spd);
-  //Serial.print("  Right: ");
+  Serial.print(mode);
   Serial.print("   :    ");
-  //Serial.print(rc_right_val);
+  if (mode == 1) {
+    Serial.print(rc_left_val);
+    Serial.print("   :    ");
+    Serial.print(rc_right_val);
+  }
+  else {
+    Serial.print(rc_speed_val);
+    Serial.print("   :    ");
+    Serial.print(rc_turn_val);
+  }
+  Serial.print("   :    ");
+  Serial.print(left_spd);
+  Serial.print("   :    ");
   Serial.print(right_spd);
   Serial.println("");
 }
